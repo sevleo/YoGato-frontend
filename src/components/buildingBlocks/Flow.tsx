c748611dab2f0cc6ba2ea410dc4dc8632a34dcd9
Seva <seva.leonov@hotmail.com>
2024-02-22 14:15:00 +0700

    Add button to create units

--- src/components/buildingBlocks/Flow.tsx
+++ src/components/buildingBlocks/Flow.tsx
@@ -1,18 +1,45 @@
+import { SetStateAction } from "react";
 import Unit from "./Unit";
 import { UnitProps } from "./Unit";
+import { Dispatch } from "react";
 
 interface FlowProps {
   flow: {
     flowName: string;
     units: UnitProps[];
   };
+  setFlow: Dispatch<
+    SetStateAction<{
+      flowName: string;
+      units: UnitProps[];
+    }>
+  >;
 }
 
-function Flow({ flow }: FlowProps) {
+function Flow({ flow, setFlow }: FlowProps) {
+  function handleButtonClick() {
+    const newUnit = {
+      name: "new unit",
+      sanskritName: "sanskrit name of unit 1",
+      duration: 5,
+      announcement: "fancy announcement",
+    };
+    console.log(newUnit);
+    setFlow((prevFlow) => {
+      return {
+        ...prevFlow,
+        units: [...prevFlow.units, newUnit],
+      };
+    });
+  }
+
   return (
     <div className="h-auto p-5">
       <div className="h-full border">
         <div>{flow.flowName}</div>
+        <div>
+          <button onClick={handleButtonClick}>Create new unit</button>
+        </div>
         <div className=" flex flex-col gap-5 bg-slate-400">
           {flow.units.map((unit) => (
             <Unit key={unit.name} {...unit}></Unit>
--- src/components/pages/Builder.tsx
+++ src/components/pages/Builder.tsx
@@ -2,6 +2,7 @@ import AspectGroup from "../buildingBlocks/AspectGroup";
 import Flow from "../buildingBlocks/Flow";
 import { UnitProps } from "../buildingBlocks/Unit";
 import { AspectGroupProps } from "../buildingBlocks/AspectGroup";
+import { useState, useEffect } from "react";
 
 function Builder() {
   const aspectGroups: AspectGroupProps[] = [
@@ -72,7 +73,7 @@ function Builder() {
     units: UnitProps[];
   }
 
-  const flow: Flow = {
+  const defaultFlow: Flow = {
     flowName: "my fancy flow",
     units: [
       {
@@ -87,74 +88,76 @@ function Builder() {
         duration: 2,
         announcement: "fancy announcement",
       },
-      {
-        name: "unit 3",
-        sanskritName: "sanskrit name of unit 3",
-        duration: 8,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 4",
-        sanskritName: "sanskrit name of unit 1",
-        duration: 5,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 5",
-        sanskritName: "sanskrit name of unit 2",
-        duration: 2,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 6",
-        sanskritName: "sanskrit name of unit 3",
-        duration: 8,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 7",
-        sanskritName: "sanskrit name of unit 1",
-        duration: 5,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 8",
-        sanskritName: "sanskrit name of unit 2",
-        duration: 2,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 9",
-        sanskritName: "sanskrit name of unit 3",
-        duration: 8,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 10",
-        sanskritName: "sanskrit name of unit 1",
-        duration: 5,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 11",
-        sanskritName: "sanskrit name of unit 2",
-        duration: 2,
-        announcement: "fancy announcement",
-      },
-      {
-        name: "unit 12",
-        sanskritName: "sanskrit name of unit 3",
-        duration: 8,
-        announcement: "fancy announcement",
-      },
+      // {
+      //   name: "unit 3",
+      //   sanskritName: "sanskrit name of unit 3",
+      //   duration: 8,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 4",
+      //   sanskritName: "sanskrit name of unit 1",
+      //   duration: 5,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 5",
+      //   sanskritName: "sanskrit name of unit 2",
+      //   duration: 2,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 6",
+      //   sanskritName: "sanskrit name of unit 3",
+      //   duration: 8,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 7",
+      //   sanskritName: "sanskrit name of unit 1",
+      //   duration: 5,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 8",
+      //   sanskritName: "sanskrit name of unit 2",
+      //   duration: 2,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 9",
+      //   sanskritName: "sanskrit name of unit 3",
+      //   duration: 8,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 10",
+      //   sanskritName: "sanskrit name of unit 1",
+      //   duration: 5,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 11",
+      //   sanskritName: "sanskrit name of unit 2",
+      //   duration: 2,
+      //   announcement: "fancy announcement",
+      // },
+      // {
+      //   name: "unit 12",
+      //   sanskritName: "sanskrit name of unit 3",
+      //   duration: 8,
+      //   announcement: "fancy announcement",
+      // },
     ],
   };
+
+  const [flow, setFlow] = useState<Flow>(defaultFlow);
 
   return (
     <>
       <div className="builder ml-auto mr-auto flex h-full w-full max-w-screen-2xl ">
         <div className="canvas w-1/2 overflow-auto bg-slate-500">
-          <Flow flow={flow}></Flow>
+          <Flow flow={flow} setFlow={setFlow}></Flow>
         </div>
 
         <div className="flex w-1/2 flex-col justify-start gap-5 overflow-auto bg-slate-300">
