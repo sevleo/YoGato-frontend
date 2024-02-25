9a6f731a82e2727d9aa45e357d28b3f4bce54638
Seva <seva.leonov@hotmail.com>
2024-02-25 16:39:23 +0700

    Remove unnecessary imports

--- src/components/buildingBlocks/AspectController.tsx
+++ src/components/buildingBlocks/AspectController.tsx
@@ -1,6 +1,5 @@
 import { useDraggable } from "@dnd-kit/core";
 import { HTMLAttributes } from "react";
-import { CSS } from "@dnd-kit/utilities";
 import Aspect from "./AspectDisplay";
 
 export interface AspectType {
@@ -12,29 +11,14 @@ type Props = {
 } & HTMLAttributes<HTMLDivElement>;
 
 export default function AspectController({ aspect }: Props) {
-  const {
-    attributes,
-    isDragging,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useDraggable({
+  const { attributes, isDragging, listeners, setNodeRef } = useDraggable({
     id: aspect.name,
   });
-
-  const styles = transform
-    ? {
-        // transform: CSS.Transform.toString(transform),
-        // transition: transition || undefined,
-      }
-    : undefined;
 
   return (
     <Aspect
       aspect={aspect}
       ref={setNodeRef}
-      style={styles}
       isOpacityEnabled={isDragging}
       isDragging={isDragging}
       {...attributes}
