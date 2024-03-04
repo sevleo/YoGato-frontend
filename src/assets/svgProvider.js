f95c1d15ce7f051880371f58914d817d328db461
Seva <seva.leonov@hotmail.com>
2024-03-04 16:13:25 +0700

    Updated and uploaded svg files

--- src/assets/svgProvider.js
+++ src/assets/svgProvider.js
@@ -1,5 +1,51 @@
 import boatstraightlegs from "./poses_images/boatstraightlegs.svg";
 import bow from "./poses_images/bow.svg";
+import bridge from "./poses_images/bridge.svg";
+import butterfly from "./poses_images/butterfly.svg";
+import camel from "./poses_images/camel.svg";
+import cat from "./poses_images/cat.svg";
+import chair from "./poses_images/chair.svg";
+import child from "./poses_images/child.svg";
+import corpse from "./poses_images/corpse.svg";
+import cow from "./poses_images/cow.svg";
+import highlunge from "./poses_images/highlunge.svg";
+import standingstretch from "./poses_images/standingstretch.svg";
+import tree from "./poses_images/tree.svg";
+import crow from "./poses_images/crow.svg";
+import dolphin from "./poses_images/dolphin.svg";
+import plank from "./poses_images/plank.svg";
+import downdog from "./poses_images/downdog.svg";
+import eagle from "./poses_images/eagle.svg";
+import extendedhandtotoe from "./poses_images/extendedhandtotoe.svg";
+import extendedsideangle from "./poses_images/extendedsideangle.svg";
+import forearmstand from "./poses_images/forearmstand.svg";
+import forwardfoldshoulderstretch from "./poses_images/forwardfoldshoulderstretch.svg";
+import squat from "./poses_images/squat.svg";
+import boatbentlegs from "./poses_images/boatbentlegs.svg";
+import seatedspinaltwist from "./poses_images/seatedspinaltwist.svg";
+import halfmoon from "./poses_images/halfmoon.svg";
+import handstand from "./poses_images/handstand.svg";
+import kingpigeon from "./poses_images/kingpigeon.svg";
+import lotus from "./poses_images/lotus.svg";
+import lowlunge from "./poses_images/lowlunge.svg";
+import pigeon from "./poses_images/pigeon.svg";
+import plow from "./poses_images/plow.svg";
+import pyramid from "./poses_images/pyramid.svg";
+import reverswarrior from "./poses_images/reverswarrior.svg";
+import seatedforwardfold from "./poses_images/seatedforwardfold.svg";
+import standingforwardfold from "./poses_images/standingforwardfold.svg";
+import triangle from "./poses_images/triangle.svg";
+import updog from "./poses_images/updog.svg";
+import warrior1 from "./poses_images/warrior1.svg";
+import warrior3 from "./poses_images/warrior3.svg";
+import warrior2 from "./poses_images/warrior2.svg";
+import wheel from "./poses_images/wheel.svg";
+import wildthing from "./poses_images/wildthing.svg";
+import shoulderstand from "./poses_images/shoulderstand.svg";
+import sideplank from "./poses_images/sideplank.svg";
+import straddlesplit from "./poses_images/straddlesplit.svg";
+import sphinx from "./poses_images/sphinx.svg";
+import splits from "./poses_images/splits.svg";
 
 function svgProvider(poseName) {
   switch (poseName) {
@@ -7,6 +53,98 @@ function svgProvider(poseName) {
       return boatstraightlegs;
     case "bow":
       return bow;
+    case "bridge":
+      return bridge;
+    case "butterfly":
+      return butterfly;
+    case "camel":
+      return camel;
+    case "cat":
+      return cat;
+    case "chair":
+      return chair;
+    case "child":
+      return child;
+    case "corpse":
+      return corpse;
+    case "cow":
+      return cow;
+    case "highlunge":
+      return highlunge;
+    case "standingstretch":
+      return standingstretch;
+    case "tree":
+      return tree;
+    case "crow":
+      return crow;
+    case "dolphin":
+      return dolphin;
+    case "plank":
+      return plank;
+    case "downdog":
+      return downdog;
+    case "eagle":
+      return eagle;
+    case "extendedhandtotoe":
+      return extendedhandtotoe;
+    case "extendedsideangle":
+      return extendedsideangle;
+    case "forearmstand":
+      return forearmstand;
+    case "forwardfoldshoulderstretch":
+      return forwardfoldshoulderstretch;
+    case "squat":
+      return squat;
+    case "boatbentlegs":
+      return boatbentlegs;
+    case "seatedspinaltwist":
+      return seatedspinaltwist;
+    case "halfmoon":
+      return halfmoon;
+    case "handstand":
+      return handstand;
+    case "kingpigeon":
+      return kingpigeon;
+    case "lotus":
+      return lotus;
+    case "lowlunge":
+      return lowlunge;
+    case "pigeon":
+      return pigeon;
+    case "plow":
+      return plow;
+    case "pyramid":
+      return pyramid;
+    case "reverswarrior":
+      return reverswarrior;
+    case "seatedforwardfold":
+      return seatedforwardfold;
+    case "standingforwardfold":
+      return standingforwardfold;
+    case "triangle":
+      return triangle;
+    case "updog":
+      return updog;
+    case "warrior1":
+      return warrior1;
+    case "warrior3":
+      return warrior3;
+    case "warrior2":
+      return warrior2;
+    case "wheel":
+      return wheel;
+    case "wildthing":
+      return wildthing;
+    case "shoulderstand":
+      return shoulderstand;
+    case "sideplank":
+      return sideplank;
+    case "straddlesplit":
+      return straddlesplit;
+    case "sphinx":
+      return sphinx;
+    case "splits":
+      return splits;
   }
 }
 
