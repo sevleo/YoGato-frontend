72e2089495f2f15a1239ab82f85380c33ffdd3f1
Seva <seva.leonov@hotmail.com>
2024-03-16 19:48:01 +0700

    Add header smooth appearance

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,27 +1,64 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 1) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className=" z-50 flex h-[60px] w-full items-center  justify-center border-b-[0.5px] border-neutral-300 bg-black text-white shadow-md">
-        <div className="flex w-full max-w-screen-2xl flex-row items-center justify-start gap-10 p-2 pl-5 text-black">
-          <Link className="text-white hover:text-white hover:underline" to="/">
-            Home
-          </Link>
-          <Link
-            className=" text-white hover:text-white hover:underline"
-            to="/builder"
-          >
-            Flow Builder
-          </Link>
-          <Link
-            className="text-white hover:text-white hover:underline"
-            to="/experiment"
-          >
-            Experiment...
-          </Link>
+      <header className=" relative z-50 flex h-[60px] w-full items-center  justify-center border-b-[0.5px] border-neutral-300 bg-black text-white shadow-md">
+        <div className="flex h-[60px] w-full max-w-screen-2xl flex-row items-center justify-start gap-10 p-2 pl-5 text-black">
+          <HeaderDetails></HeaderDetails>
+        </div>
+        <div
+          className={`fixed z-10 flex h-[60px] w-full max-w-screen-2xl flex-row items-center justify-start gap-10 bg-black p-2 pl-5`}
+          style={{
+            background: "rgba(34, 32, 30, 0.9)",
+            top: isScrolled ? "0px" : "-60px",
+            transition: "top 0.3s ease-in-out",
+          }}
+        >
+          <HeaderDetails></HeaderDetails>
         </div>
       </header>
+    </>
+  );
+}
+
+function HeaderDetails() {
+  return (
+    <>
+      <Link className="text-white hover:text-white hover:underline" to="/">
+        Home
+      </Link>
+      <Link
+        className=" text-white hover:text-white hover:underline"
+        to="/builder"
+      >
+        Flow Builder
+      </Link>
+      <Link
+        className="text-white hover:text-white hover:underline"
+        to="/experiment"
+      >
+        Experiment...
+      </Link>
     </>
   );
 }
