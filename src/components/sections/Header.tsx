ac80b36498c5d5854c6d8262a8a3caa4369b6a5d
Seva <seva.leonov@hotmail.com>
2024-03-16 20:02:39 +0700

    Update shadow style

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 
   useEffect(() => {
     function handleScroll() {
-      if (window.scrollY > 1) {
+      if (window.scrollY > 5) {
         setIsScrolled(true);
       } else {
         setIsScrolled(false);
@@ -22,12 +22,16 @@ export default function Header() {
 
   return (
     <>
-      <header className=" relative z-50 flex h-[60px] w-full items-center  justify-center border-b-[0.5px] border-neutral-300 bg-black text-white shadow-md">
-        <div className="flex h-[60px] w-full max-w-screen-2xl flex-row items-center justify-start gap-10 p-2 pl-5 text-black">
+      <header
+        className={`} relative z-50 flex h-[60px] w-full  items-center justify-center border-neutral-300 bg-black text-white`}
+      >
+        <div
+          className={`flex h-[60px] w-full max-w-screen-2xl flex-row items-center justify-start gap-10 p-2 pl-5 text-black ${isScrolled ? "blur-sm" : ""} shadow-lg`}
+        >
           <HeaderDetails></HeaderDetails>
         </div>
         <div
-          className={`fixed z-10 flex h-[60px] w-full max-w-screen-2xl flex-row items-center justify-start gap-10 bg-black p-2 pl-5`}
+          className={`fixed z-10 flex h-[60px] w-full max-w-screen-2xl flex-row items-center justify-start gap-10 bg-black p-2 pl-5 shadow-lg`}
           style={{
             background: "rgba(34, 32, 30, 0.9)",
             top: isScrolled ? "0px" : "-60px",
