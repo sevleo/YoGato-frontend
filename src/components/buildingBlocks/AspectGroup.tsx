93cb1aab4f036b8a9585d06d3a3c4a11918b854d
Seva <seva.leonov@hotmail.com>
2024-03-18 14:59:27 +0700

    Update accordion

--- src/components/buildingBlocks/AspectGroup.tsx
+++ src/components/buildingBlocks/AspectGroup.tsx
@@ -7,6 +7,14 @@ import { AspectType } from "./Aspect/AspectController";
 import { Dispatch, SetStateAction } from "react";
 import { UnitType } from "./Unit";
 
+import Accordion, { AccordionSlots } from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Fade from "@mui/material/Fade";
+import { useState } from "react";
+
 export interface AspectGroupType {
   category_name: string;
   poses: AspectType[];
@@ -38,30 +46,53 @@ function AspectGroup({
   uniqueAspects,
   setFlow,
 }: AspectGroupProps) {
-  // console.log(uniqueAspects);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpansion = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  };
   return (
-    <div className=" flex flex-col justify-center text-black">
-      <p className="mb-2 mt-5 text-white ">{category_name}</p>
-      {/* <div className="grid grid-cols-aspects justify-center gap-5"> */}
-      <div className="auto-rows grid grid-cols-aspects grid-rows-aspects justify-center gap-5">
-        {poses.map((pose) => {
-          let count = 0;
-          uniqueAspects.some((uniqueAspect) => {
-            if (uniqueAspect.id === pose.id) {
-              count = uniqueAspect.count;
-            }
-          });
-          return (
-            <AspectController
-              key={pose.english_name + pose.category_name}
-              aspect={pose}
-              count={count}
-              setFlow={setFlow}
-            ></AspectController>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <Accordion
+        expanded={expanded}
+        onChange={handleExpansion}
+        slots={{ transition: Fade as AccordionSlots["transition"] }}
+        slotProps={{ transition: { timeout: 400 } }}
+        sx={{
+          "& .MuiAccordion-region": { height: expanded ? "auto" : 0 },
+          "& .MuiAccordionDetails-root": {
+            display: expanded ? "block" : "none",
+          },
+        }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>{category_name}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div className=" flex flex-col justify-center text-black">
+            {/* <div className="grid grid-cols-aspects justify-center gap-5"> */}
+            <div className="auto-rows grid grid-cols-aspects grid-rows-aspects justify-center gap-5">
+              {poses.map((pose) => {
+                let count = 0;
+                uniqueAspects.some((uniqueAspect) => {
+                  if (uniqueAspect.id === pose.id) {
+                    count = uniqueAspect.count;
+                  }
+                });
+                return (
+                  <AspectController
+                    key={pose.english_name + pose.category_name}
+                    aspect={pose}
+                    count={count}
+                    setFlow={setFlow}
+                  ></AspectController>
+                );
+              })}
+            </div>
+          </div>
+        </AccordionDetails>
+      </Accordion>
+    </>
   );
 }
 
--- src/components/sections/AspectCollection.tsx
+++ src/components/sections/AspectCollection.tsx
@@ -3,6 +3,13 @@ import { AspectGroupType } from "../buildingBlocks/AspectGroup";
 import { FlowType } from "./Flow";
 import { Dispatch, SetStateAction } from "react";
 import { UnitType } from "../buildingBlocks/Unit";
+import Accordion, { AccordionSlots } from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Fade from "@mui/material/Fade";
+import { useState } from "react";
 
 interface AspectCollectionProps {
   aspectGroups: AspectGroupType[];
@@ -25,6 +32,12 @@ export default function AspectCollection({
   flow,
   setFlow,
 }: AspectCollectionProps) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpansion = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  };
+
   return (
     <div className="flex  w-1/3 flex-col justify-start bg-[#ffffff18] pb-[40px] pl-[40px] pr-[40px]">
       {/* <div>
@@ -32,15 +45,17 @@ export default function AspectCollection({
       </div> */}
       <div className="h-fit min-h-full w-full gap-5">
         {/* <div className=" border-[1px] border-solid border-neutral-200 p-5"> */}
-        <div className="">
+        <div className="accordion">
           {aspectGroups.map((aspectGroup) => (
-            <AspectGroup
-              key={aspectGroup.category_name}
-              category_name={aspectGroup.category_name}
-              poses={aspectGroup.poses}
-              uniqueAspects={flow.uniqueAspects}
-              setFlow={setFlow}
-            ></AspectGroup>
+            <>
+              <AspectGroup
+                key={aspectGroup.category_name}
+                category_name={aspectGroup.category_name}
+                poses={aspectGroup.poses}
+                uniqueAspects={flow.uniqueAspects}
+                setFlow={setFlow}
+              ></AspectGroup>
+            </>
           ))}
         </div>
       </div>
