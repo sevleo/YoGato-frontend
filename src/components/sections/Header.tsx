a26f1a4c8acb78ddc6907f2914112464053695d2
Seva <seva.leonov@hotmail.com>
2024-03-23 16:52:35 +0700

    Update header style bugs

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -63,7 +63,7 @@ export default function Header({
         className={` relative z-50 flex h-[60px] w-full  items-center justify-center border-neutral-300 bg-black text-white`}
       >
         <div
-          className={`relative flex h-[60px] w-full max-w-screen-2xl flex-row items-center justify-start gap-10 pl-5 pr-5 text-black ${isScrolled ? "blur-sm" : ""} shadow-lg`}
+          className={`relative flex h-[60px] w-full flex-row items-center justify-center gap-10 pl-5 pr-5 text-black ${isScrolled ? "blur-sm" : ""} shadow-lg`}
         >
           <HeaderDetails
             enableHamburger={enableHamburger}
@@ -79,7 +79,7 @@ export default function Header({
           ></HeaderDetails>
         </div>
         <div
-          className={`fixed left-[-1px] z-10 flex h-[60px] w-full flex-row items-center justify-center gap-10 bg-black pl-5 pr-5 shadow-lg`}
+          className={`fixed left-[-1px] z-10  flex h-[60px] w-full flex-row items-center justify-center gap-10 bg-black pl-5 pr-5 shadow-lg`}
           style={{
             background: "rgba(34, 32, 30, 0.9)",
             top: isScrolled ? "0px" : "-60px",
@@ -150,7 +150,7 @@ function HeaderDetails({
 
   return (
     <>
-      <div className="w-full max-w-screen-2xl">
+      <div className="flex w-full max-w-screen-2xl items-center justify-start gap-10">
         {isHamburgerMenu ? (
           <>
             <Link
