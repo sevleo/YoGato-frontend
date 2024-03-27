62c1d7c3531e59077046bfe4e50ed8c109a6a7e1
Seva <seva.leonov@hotmail.com>
2024-03-27 22:29:40 +0700

    Update log-in

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -8,19 +8,45 @@ export default function SignIn({
   location,
   setLocation,
 }) {
-  const [signUpPage, setSignUpPage] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    async function fetchSignUpPage() {
-      try {
-        const response = await axios.get("http://localhost:3001/sign-up");
-        setSignUpPage(response.data);
-      } catch (error) {
-        console.error("Error fetching sign-up page:", error);
-      }
+  async function handleLogin(event) {
+    event.preventDefault();
+    console.log(event);
+    try {
+      const response = await axios.post("http://localhost:3001/log-in", {
+        username: username,
+        password: password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.error("Error logging in:", error);
     }
-    fetchSignUpPage();
-  }, []);
+  }
+
+  // async function handleLogin(event) {
+  //   event.preventDefault();
+
+  //   try {
+  //     const response = await fetch("http://localhost:3001/log-in", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ username: username, password: password }),
+  //     });
+
+  //     if (!response.ok) {
+  //       console.log(response);
+  //       throw new Error("Login failed");
+  //     }
+
+  //     window.location.href = "/";
+  //   } catch (error) {
+  //     console.error("Error logging in:", error);
+  //   }
+  // }
 
   return (
     <>
@@ -32,7 +58,28 @@ export default function SignIn({
       />
       <div>
         <h2>Log In</h2>
-        <div dangerouslySetInnerHTML={{ __html: signUpPage }} />
+        <div>
+          <form method="POST" onSubmit={handleLogin}>
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              name="username"
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Log In</button>
+          </form>
+        </div>
       </div>
     </>
   );
