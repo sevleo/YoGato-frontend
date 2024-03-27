c4b3b20cf627d0d2115fcad2c8942507a8525379
Seva <seva.leonov@hotmail.com>
2024-03-28 00:13:26 +0700

    Add login-logout

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -10,18 +10,52 @@ export default function SignIn({
 }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  async function checkLoggedIn() {
+    try {
+      const response = await axios.get("http://localhost:3001/check-login", {
+        withCredentials: true,
+      });
+      console.log(response);
+      setIsLoggedIn(true);
+    } catch (error) {
+      setIsLoggedIn(false);
+    }
+  }
+
+  useEffect(() => {
+    checkLoggedIn();
+  }, []);
 
   async function handleLogin(event) {
     event.preventDefault();
     console.log(event);
     try {
-      const response = await axios.post("http://localhost:3001/log-in", {
-        username: username,
-        password: password,
-      });
+      const response = await axios.post(
+        "http://localhost:3001/log-in",
+        { username: username, password: password },
+        {
+          withCredentials: true,
+        }
+      );
       console.log(response);
+      setIsLoggedIn(true);
     } catch (error) {
       console.error("Error logging in:", error);
+      setIsLoggedIn(false);
+    }
+  }
+
+  async function handleLogout(event) {
+    try {
+      const response = await axios.get("http://localhost:3001/log-out", {
+        withCredentials: true,
+      });
+      console.log(response);
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.error("Error logging out:", error);
     }
   }
 
@@ -57,29 +91,41 @@ export default function SignIn({
         setLocation={setLocation}
       />
       <div>
-        <h2>Log In</h2>
-        <div>
-          <form method="POST" onSubmit={handleLogin}>
-            <label htmlFor="username">Username</label>
-            <input
-              id="username"
-              name="username"
-              placeholder="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Log In</button>
-          </form>
-        </div>
+        {/* <button onClick={checkLoggedIn}>Check log in</button> */}
+        {isLoggedIn ? (
+          <div>
+            <p>You are logged in</p>
+            <button onClick={handleLogout}>Log out</button>
+          </div>
+        ) : (
+          <div>
+            <p>You are NOT logged in</p>
+          </div>
+        )}
+        {!isLoggedIn ? (
+          <div>
+            <form method="POST" onSubmit={handleLogin}>
+              <label htmlFor="username">Username</label>
+              <input
+                id="username"
+                name="username"
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Log In</button>
+            </form>
+          </div>
+        ) : null}
       </div>
     </>
   );
