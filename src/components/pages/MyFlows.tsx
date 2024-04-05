541e27586f3be686c4ebe65491c81156aab17cc4
Seva <seva.leonov@hotmail.com>
2024-04-05 17:34:19 +0700

    Add create flow button integrated with MongoDB

--- src/components/pages/MyFlows.tsx
+++ src/components/pages/MyFlows.tsx
@@ -1,7 +1,24 @@
+import axios from "axios";
+
 function MyFlows() {
+  async function handleNewFlowClick(event) {
+    try {
+      const response = await axios.post("http://localhost:3001/new-flow");
+    } catch (error) {
+      console.error("Error adding flow:", error);
+    }
+  }
+
   return (
     <>
-      <div className="my-flows">My Flows</div>
+      <div className="my-flows ml-auto mr-auto flex w-full max-w-screen-2xl justify-center pt-[20px] ">
+        <div className="w-3/4">
+          <div className=" ml-auto mr-auto h-[500px] w-full items-start justify-center bg-[#ffffff18] text-white transition-colors hover:bg-[#ffffff38]">
+            <p>Flows</p>
+            <button onClick={handleNewFlowClick}>New Flow</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
