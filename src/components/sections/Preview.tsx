4ebceaea5c66a2f707c98a85253026a9c8aa31c3
Seva <seva.leonov@hotmail.com>
2024-04-08 15:13:26 +0700

    Add comment

--- src/components/sections/Preview.tsx
+++ src/components/sections/Preview.tsx
@@ -24,6 +24,7 @@ function Preview() {
   const { flow } = useFlow();
   const { authState } = useUser();
 
+  // Redirect from preview to builder when flow has no units
   useEffect(() => {
     if (flow.units.length === 0) {
       navigate("/builder");
