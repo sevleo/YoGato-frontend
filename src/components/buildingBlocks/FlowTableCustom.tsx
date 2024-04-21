10b4c633b6aad7cda08807799a72cf2e11017457
Seva <seva.leonov@hotmail.com>
2024-04-21 14:54:37 +0700

    Update styles of my flows table and icons

--- src/components/buildingBlocks/FlowTableCustom.tsx
+++ src/components/buildingBlocks/FlowTableCustom.tsx
@@ -46,16 +46,18 @@ export default function FlowTableCustom({
           </tr>
         </thead>
         <tbody>
-          {flows.map((flow) => (
-            <TableRow
-              key={flow._id}
-              flow={flow}
-              handleDelete={handleDelete}
-              showAllFlows={showAllFlows}
-              navigate={navigate}
-              setFlow={setFlow}
-            ></TableRow>
-          ))}
+          {flows.map((flow) => {
+            return (
+              <TableRow
+                key={flow._id}
+                flow={flow}
+                handleDelete={handleDelete}
+                showAllFlows={showAllFlows}
+                navigate={navigate}
+                setFlow={setFlow}
+              ></TableRow>
+            );
+          })}
         </tbody>
       </table>
     </>
@@ -77,6 +79,12 @@ function TableRow({
   navigate,
   setFlow,
 }: TableRow) {
+  console.log(flow.flowData);
+  const duration = flow.flowData.duration;
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = duration % 60;
+
   const [editable, setEditable] = useState(false);
   const [editedFlowName, setEditedFlowName] = useState(flow.flowName);
 
@@ -122,7 +130,13 @@ function TableRow({
         )}
       </TableData>
       <TableData style=" w-fit">
-        {flow.flowData.duration ? flow.flowData.duration : 0}
+        <p className="">
+          {hours > 0 ? <span className=" font-medium">{hours}h, </span> : null}
+          {minutes > 0 ? (
+            <span className="font-medium">{minutes}m, </span>
+          ) : null}
+          {seconds > 0 ? <span className="font-medium">{seconds}s</span> : null}
+        </p>
       </TableData>
       <TableData style=" w-fit">
         {flow.flowData.units ? flow.flowData.units.length : 0}
@@ -133,7 +147,7 @@ function TableRow({
       </TableData>
 
       <TableData style="border-r-[1px] w-fit">
-        <div className="flex gap-2">
+        <div className="flex items-center justify-center gap-2">
           <Button
             componentType="myFlowsPreview"
             onClick={() => handlePreviewClick(flow._id)}
