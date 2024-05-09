005d45b5dc890ff63ca78693b114bc3fadf57e3f
Seva <seva.leonov@hotmail.com>
2024-05-09 12:33:34 +0700

    Update clear logic

--- src/components/sections/Builder.tsx
+++ src/components/sections/Builder.tsx
@@ -22,6 +22,7 @@ function Builder({ handleMovingClick, showAllFlows, isMobile }: BuilderProps) {
   const { flow, setFlow } = useFlow();
 
   const [editableName, setEditableName] = useState<boolean>(false);
+  const [clearClicked, setClearClicked] = useState<boolean>(false);
 
   const handlePreviewButtonClick: ClickHandler = () => {
     handleMovingClick();
@@ -69,6 +70,7 @@ function Builder({ handleMovingClick, showAllFlows, isMobile }: BuilderProps) {
     setEditedFlowName("");
     setEnableSave(false);
     setNameErrorMessage("");
+    setClearClicked(true);
   };
 
   const [nameErrorMessage, setNameErrorMessage] = useState("");
