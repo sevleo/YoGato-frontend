c9a4d11c2ac925b03edffc7fafe839d6daa69345
Seva <seva.leonov@hotmail.com>
2024-05-13 12:58:43 +0700

    Update Catalog for Mobile view

--- src/components/sections/Catalog.tsx
+++ src/components/sections/Catalog.tsx
@@ -20,7 +20,7 @@ function Catalog() {
           const image = svgProvider(aspect.url_svg_alt_local);
           return (
             <>
-              <div className="flex max-w-[700px]">
+              <div className="hidden max-w-[700px] min-[570px]:flex">
                 <div className="flex w-[200px] items-center justify-center pl-4 pr-10">
                   <img src={image} alt="" />
                 </div>
@@ -35,6 +35,40 @@ function Catalog() {
                     {aspect.translation_name}
                   </p>
                   <br />
+                  <div className="flex h-full w-full flex-col ">
+                    <div className="flex w-full gap-2">
+                      <p className="min-w-[80px] text-end text-[#a0a0a0]">
+                        Instruction
+                      </p>
+                      <div className="h-full w-[1px] bg-[#323232]"></div>
+                      <p className="w-full pb-4 text-start text-[#a0a0a0]">
+                        {aspect.pose_description}
+                      </p>
+                    </div>
+                    <div className="flex w-full gap-2">
+                      <p className="min-w-[80px] text-end text-[#a0a0a0]">
+                        Benefits
+                      </p>
+                      <div className="h-full w-[1px] bg-[#323232]"></div>
+
+                      <p className="w-full pb-4 text-start text-[#a0a0a0]">
+                        {aspect.pose_benefits}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex max-w-[700px] min-[570px]:hidden">
+                <div className="w-full">
+                  <div className="pl-[5px] text-start text-[30px]">
+                    {aspect.english_name} | {aspect.sanskrit_name}{" "}
+                  </div>
+                  <p className="pl-[5px] text-start">
+                    {aspect.translation_name}
+                  </p>
+                  <div className="flex w-[200px] items-center justify-center pb-4 pt-4">
+                    <img src={image} alt="" />
+                  </div>
                   <div className="flex h-full w-full flex-col ">
                     <div className="flex w-full gap-2">
                       <p className="min-w-[80px] text-end text-[#a0a0a0]">
