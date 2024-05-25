d007299881b5700f778db7310baf59f288f8c2aa
Seva <seva.leonov@hotmail.com>
2024-05-25 16:30:13 +0700

    Update poses to add right/left sides

--- src/assets/mp3Provider.js
+++ src/assets/mp3Provider.js
@@ -8,44 +8,94 @@ import chair from "./poses_audios/chair.mp3";
 import child from "./poses_audios/child.mp3";
 import corpse from "./poses_audios/corpse.mp3";
 import cow from "./poses_audios/cow.mp3";
-import highlunge from "./poses_audios/highlunge.mp3";
+// import highlunge from "./poses_audios/highlunge.mp3";
+import highlunge_right from "./poses_audios/highlunge_right.mp3";
+import highlunge_left from "./poses_audios/highlunge_left.mp3";
+
 import standingstretch from "./poses_audios/standingstretch.mp3";
-import tree from "./poses_audios/tree.mp3";
+// import tree from "./poses_audios/tree.mp3";
+import tree_right from "./poses_audios/tree_right.mp3";
+import tree_left from "./poses_audios/tree_left.mp3";
+
 import crow from "./poses_audios/crow.mp3";
 import dolphin from "./poses_audios/dolphin.mp3";
 import plank from "./poses_audios/plank.mp3";
 import downdog from "./poses_audios/downdog.mp3";
-import eagle from "./poses_audios/eagle.mp3";
-import extendedhandtotoe from "./poses_audios/extendedhandtotoe.mp3";
-import extendedsideangle from "./poses_audios/extendedsideangle.mp3";
+// import eagle from "./poses_audios/eagle.mp3";
+import eagle_right from "./poses_audios/eagle_right.mp3";
+import eagle_left from "./poses_audios/eagle_left.mp3";
+
+// import extendedhandtotoe from "./poses_audios/extendedhandtotoe.mp3";
+import extendedhandtotoe_right from "./poses_audios/extendedhandtotoe_right.mp3";
+import extendedhandtotoe_left from "./poses_audios/extendedhandtotoe_left.mp3";
+// import extendedsideangle from "./poses_audios/extendedsideangle.mp3";
+import extendedsideangle_right from "./poses_audios/extendedsideangle_right.mp3";
+import extendedsideangle_left from "./poses_audios/extendedsideangle_left.mp3";
+
 import forearmstand from "./poses_audios/forearmstand.mp3";
 import forwardfoldshoulderstretch from "./poses_audios/forwardfoldshoulderstretch.mp3";
 import squat from "./poses_audios/squat.mp3";
 import boatbentlegs from "./poses_audios/boatbentlegs.mp3";
-import seatedspinaltwist from "./poses_audios/seatedspinaltwist.mp3";
-import halfmoon from "./poses_audios/halfmoon.mp3";
+// import seatedspinaltwist from "./poses_audios/seatedspinaltwist.mp3";
+import seatedspinaltwist_right from "./poses_audios/seatedspinaltwist_right.mp3";
+import seatedspinaltwist_left from "./poses_audios/seatedspinaltwist_left.mp3";
+// import halfmoon from "./poses_audios/halfmoon.mp3";
+import halfmoon_right from "./poses_audios/halfmoon_right.mp3";
+import halfmoon_left from "./poses_audios/halfmoon_left.mp3";
+
 import handstand from "./poses_audios/handstand.mp3";
-import kingpigeon from "./poses_audios/kingpigeon.mp3";
+// import kingpigeon from "./poses_audios/kingpigeon.mp3";
+import kingpigeon_right from "./poses_audios/kingpigeon_right.mp3";
+import kingpigeon_left from "./poses_audios/kingpigeon_left.mp3";
 import lotus from "./poses_audios/lotus.mp3";
-import lowlunge from "./poses_audios/lowlunge.mp3";
-import pigeon from "./poses_audios/pigeon.mp3";
+// import lowlunge from "./poses_audios/lowlunge.mp3";
+import lowlunge_right from "./poses_audios/lowlunge_right.mp3";
+import lowlunge_left from "./poses_audios/lowlunge_left.mp3";
+
+// import pigeon from "./poses_audios/pigeon.mp3";
+import pigeon_right from "./poses_audios/pigeon_right.mp3";
+import pigeon_left from "./poses_audios/pigeon_left.mp3";
+
 import plow from "./poses_audios/plow.mp3";
-import pyramid from "./poses_audios/pyramid.mp3";
-import reverswarrior from "./poses_audios/reverswarrior.mp3";
+// import pyramid from "./poses_audios/pyramid.mp3";
+import pyramid_right from "./poses_audios/pyramid_right.mp3";
+import pyramid_left from "./poses_audios/pyramid_left.mp3";
+// import reverswarrior from "./poses_audios/reverswarrior.mp3";
+import reverswarrior_right from "./poses_audios/reverswarrior_right.mp3";
+import reverswarrior_left from "./poses_audios/reverswarrior_left.mp3";
+
 import seatedforwardfold from "./poses_audios/seatedforwardfold.mp3";
 import standingforwardfold from "./poses_audios/standingforwardfold.mp3";
-import triangle from "./poses_audios/triangle.mp3";
+// import triangle from "./poses_audios/triangle.mp3";
+import triangle_right from "./poses_audios/triangle_right.mp3";
+import triangle_left from "./poses_audios/triangle_left.mp3";
 import updog from "./poses_audios/updog.mp3";
-import warrior1 from "./poses_audios/warrior1.mp3";
-import warrior3 from "./poses_audios/warrior3.mp3";
-import warrior2 from "./poses_audios/warrior2.mp3";
+// import warrior1 from "./poses_audios/warrior1.mp3";
+import warrior1_right from "./poses_audios/warrior1_right.mp3";
+import warrior1_left from "./poses_audios/warrior1_left.mp3";
+
+// import warrior3 from "./poses_audios/warrior3.mp3";
+import warrior3_right from "./poses_audios/warrior3_right.mp3";
+import warrior3_left from "./poses_audios/warrior3_left.mp3";
+
+// import warrior2 from "./poses_audios/warrior2.mp3";
+import warrior2_right from "./poses_audios/warrior2_right.mp3";
+import warrior2_left from "./poses_audios/warrior2_left.mp3";
+
 import wheel from "./poses_audios/wheel.mp3";
-import wildthing from "./poses_audios/wildthing.mp3";
+// import wildthing from "./poses_audios/wildthing.mp3";
+import wildthing_right from "./poses_audios/wildthing_right.mp3";
+import wildthing_left from "./poses_audios/wildthing_left.mp3";
+
 import shoulderstand from "./poses_audios/shoulderstand.mp3";
-import sideplank from "./poses_audios/sideplank.mp3";
+// import sideplank from "./poses_audios/sideplank.mp3";
+import sideplank_right from "./poses_audios/sideplank_right.mp3";
+import sideplank_left from "./poses_audios/sideplank_left.mp3";
 import straddlesplit from "./poses_audios/straddlesplit.mp3";
 import sphinx from "./poses_audios/sphinx.mp3";
-import splits from "./poses_audios/splits.mp3";
+// import splits from "./poses_audios/splits.mp3";
+import splits_right from "./poses_audios/splits_right.mp3";
+import splits_left from "./poses_audios/splits_left.mp3";
 
 const poseAudios = {
   boatstraightlegs,
@@ -58,44 +108,63 @@ const poseAudios = {
   child,
   corpse,
   cow,
-  highlunge,
+  highlunge_right,
+  highlunge_left,
   standingstretch,
-  tree,
+  tree_right,
+  tree_left,
   crow,
   dolphin,
   plank,
   downdog,
-  eagle,
-  extendedhandtotoe,
-  extendedsideangle,
+  eagle_right,
+  eagle_left,
+  extendedhandtotoe_right,
+  extendedhandtotoe_left,
+  extendedsideangle_right,
+  extendedsideangle_left,
   forearmstand,
   forwardfoldshoulderstretch,
   squat,
   boatbentlegs,
-  seatedspinaltwist,
-  halfmoon,
+  seatedspinaltwist_right,
+  seatedspinaltwist_left,
+  halfmoon_right,
+  halfmoon_left,
   handstand,
-  kingpigeon,
+  kingpigeon_right,
+  kingpigeon_left,
   lotus,
-  lowlunge,
-  pigeon,
+  lowlunge_right,
+  lowlunge_left,
+  pigeon_right,
+  pigeon_left,
   plow,
-  pyramid,
-  reverswarrior,
+  pyramid_right,
+  pyramid_left,
+  reverswarrior_right,
+  reverswarrior_left,
   seatedforwardfold,
   standingforwardfold,
-  triangle,
+  triangle_right,
+  triangle_left,
   updog,
-  warrior1,
-  warrior3,
-  warrior2,
+  warrior1_right,
+  warrior1_left,
+  warrior3_right,
+  warrior3_left,
+  warrior2_right,
+  warrior2_left,
   wheel,
-  wildthing,
+  wildthing_right,
+  wildthing_left,
   shoulderstand,
-  sideplank,
+  sideplank_right,
+  sideplank_left,
   straddlesplit,
   sphinx,
-  splits,
+  splits_right,
+  splits_left,
 };
 
 function mp3Provider(poseName) {
--- src/assets/svgProvider.js
+++ src/assets/svgProvider.js
@@ -8,44 +8,88 @@ import chair from "./poses_images/chair.svg";
 import child from "./poses_images/child.svg";
 import corpse from "./poses_images/corpse.svg";
 import cow from "./poses_images/cow.svg";
-import highlunge from "./poses_images/highlunge.svg";
+// import highlunge from "./poses_images/highlunge.svg";
+import highlunge_right from "./poses_images/highlunge_right.svg";
+import highlunge_left from "./poses_images/highlunge_left.svg";
+
 import standingstretch from "./poses_images/standingstretch.svg";
-import tree from "./poses_images/tree.svg";
+// import tree from "./poses_images/tree.svg";
+import tree_right from "./poses_images/tree_right.svg";
+import tree_left from "./poses_images/tree_left.svg";
+
 import crow from "./poses_images/crow.svg";
 import dolphin from "./poses_images/dolphin.svg";
 import plank from "./poses_images/plank.svg";
 import downdog from "./poses_images/downdog.svg";
-import eagle from "./poses_images/eagle.svg";
-import extendedhandtotoe from "./poses_images/extendedhandtotoe.svg";
-import extendedsideangle from "./poses_images/extendedsideangle.svg";
+// import eagle from "./poses_images/eagle.svg";
+import eagle_right from "./poses_images/eagle_right.svg";
+import eagle_left from "./poses_images/eagle_left.svg";
+// import extendedhandtotoe from "./poses_images/extendedhandtotoe.svg";
+import extendedhandtotoe_right from "./poses_images/extendedhandtotoe_right.svg";
+import extendedhandtotoe_left from "./poses_images/extendedhandtotoe_left.svg";
+// import extendedsideangle from "./poses_images/extendedsideangle.svg";
+import extendedsideangle_right from "./poses_images/extendedsideangle_right.svg";
+import extendedsideangle_left from "./poses_images/extendedsideangle_left.svg";
 import forearmstand from "./poses_images/forearmstand.svg";
 import forwardfoldshoulderstretch from "./poses_images/forwardfoldshoulderstretch.svg";
 import squat from "./poses_images/squat.svg";
 import boatbentlegs from "./poses_images/boatbentlegs.svg";
-import seatedspinaltwist from "./poses_images/seatedspinaltwist.svg";
-import halfmoon from "./poses_images/halfmoon.svg";
+// import seatedspinaltwist from "./poses_images/seatedspinaltwist.svg";
+import seatedspinaltwist_right from "./poses_images/seatedspinaltwist_right.svg";
+import seatedspinaltwist_left from "./poses_images/seatedspinaltwist_left.svg";
+// import halfmoon from "./poses_images/halfmoon.svg";
+import halfmoon_right from "./poses_images/halfmoon_right.svg";
+import halfmoon_left from "./poses_images/halfmoon_left.svg";
+
 import handstand from "./poses_images/handstand.svg";
-import kingpigeon from "./poses_images/kingpigeon.svg";
+// import kingpigeon from "./poses_images/kingpigeon.svg";
+import kingpigeon_right from "./poses_images/kingpigeon_right.svg";
+import kingpigeon_left from "./poses_images/kingpigeon_left.svg";
 import lotus from "./poses_images/lotus.svg";
-import lowlunge from "./poses_images/lowlunge.svg";
-import pigeon from "./poses_images/pigeon.svg";
+// import lowlunge from "./poses_images/lowlunge.svg";
+import lowlunge_right from "./poses_images/lowlunge_right.svg";
+import lowlunge_left from "./poses_images/lowlunge_left.svg";
+
+// import pigeon from "./poses_images/pigeon.svg";
+import pigeon_right from "./poses_images/pigeon_right.svg";
+import pigeon_left from "./poses_images/pigeon_left.svg";
+
 import plow from "./poses_images/plow.svg";
-import pyramid from "./poses_images/pyramid.svg";
-import reverswarrior from "./poses_images/reverswarrior.svg";
+// import pyramid from "./poses_images/pyramid.svg";
+import pyramid_right from "./poses_images/pyramid_right.svg";
+import pyramid_left from "./poses_images/pyramid_left.svg";
+// import reverswarrior from "./poses_images/reverswarrior.svg";
+import reverswarrior_right from "./poses_images/reverswarrior_right.svg";
+import reverswarrior_left from "./poses_images/reverswarrior_left.svg";
+
 import seatedforwardfold from "./poses_images/seatedforwardfold.svg";
 import standingforwardfold from "./poses_images/standingforwardfold.svg";
-import triangle from "./poses_images/triangle.svg";
+// import triangle from "./poses_images/triangle.svg";
+import triangle_right from "./poses_images/triangle_right.svg";
+import triangle_left from "./poses_images/triangle_left.svg";
 import updog from "./poses_images/updog.svg";
-import warrior1 from "./poses_images/warrior1.svg";
-import warrior3 from "./poses_images/warrior3.svg";
-import warrior2 from "./poses_images/warrior2.svg";
+// import warrior1 from "./poses_images/warrior1.svg";
+import warrior1_right from "./poses_images/warrior1_right.svg";
+import warrior1_left from "./poses_images/warrior1_left.svg";
+// import warrior3 from "./poses_images/warrior3.svg";
+import warrior3_right from "./poses_images/warrior3_right.svg";
+import warrior3_left from "./poses_images/warrior3_left.svg";
+// import warrior2 from "./poses_images/warrior2.svg";
+import warrior2_right from "./poses_images/warrior2_right.svg";
+import warrior2_left from "./poses_images/warrior2_left.svg";
 import wheel from "./poses_images/wheel.svg";
-import wildthing from "./poses_images/wildthing.svg";
+// import wildthing from "./poses_images/wildthing.svg";
+import wildthing_right from "./poses_images/wildthing_right.svg";
+import wildthing_left from "./poses_images/wildthing_left.svg";
 import shoulderstand from "./poses_images/shoulderstand.svg";
-import sideplank from "./poses_images/sideplank.svg";
+// import sideplank from "./poses_images/sideplank.svg";
+import sideplank_right from "./poses_images/sideplank_right.svg";
+import sideplank_left from "./poses_images/sideplank_left.svg";
 import straddlesplit from "./poses_images/straddlesplit.svg";
 import sphinx from "./poses_images/sphinx.svg";
-import splits from "./poses_images/splits.svg";
+// import splits from "./poses_images/splits.svg";
+import splits_right from "./poses_images/splits_right.svg";
+import splits_left from "./poses_images/splits_left.svg";
 
 function svgProvider(poseName) {
   switch (poseName) {
@@ -69,12 +113,16 @@ function svgProvider(poseName) {
       return corpse;
     case "cow":
       return cow;
-    case "highlunge":
-      return highlunge;
+    case "highlunge_right":
+      return highlunge_right;
+    case "highlunge_left":
+      return highlunge_left;
     case "standingstretch":
       return standingstretch;
-    case "tree":
-      return tree;
+    case "tree_right":
+      return tree_right;
+    case "tree_left":
+      return tree_left;
     case "crow":
       return crow;
     case "dolphin":
@@ -83,12 +131,18 @@ function svgProvider(poseName) {
       return plank;
     case "downdog":
       return downdog;
-    case "eagle":
-      return eagle;
-    case "extendedhandtotoe":
-      return extendedhandtotoe;
-    case "extendedsideangle":
-      return extendedsideangle;
+    case "eagle_right":
+      return eagle_right;
+    case "eagle_left":
+      return eagle_left;
+    case "extendedhandtotoe_right":
+      return extendedhandtotoe_right;
+    case "extendedhandtotoe_left":
+      return extendedhandtotoe_left;
+    case "extendedsideangle_right":
+      return extendedsideangle_right;
+    case "extendedsideangle_left":
+      return extendedsideangle_left;
     case "forearmstand":
       return forearmstand;
     case "forwardfoldshoulderstretch":
@@ -97,54 +151,82 @@ function svgProvider(poseName) {
       return squat;
     case "boatbentlegs":
       return boatbentlegs;
-    case "seatedspinaltwist":
-      return seatedspinaltwist;
-    case "halfmoon":
-      return halfmoon;
+    case "seatedspinaltwist_right":
+      return seatedspinaltwist_right;
+    case "seatedspinaltwist_left":
+      return seatedspinaltwist_left;
+    case "halfmoon_right":
+      return halfmoon_right;
+    case "halfmoon_left":
+      return halfmoon_left;
     case "handstand":
       return handstand;
-    case "kingpigeon":
-      return kingpigeon;
+    case "kingpigeon_right":
+      return kingpigeon_right;
+    case "kingpigeon_left":
+      return kingpigeon_left;
     case "lotus":
       return lotus;
-    case "lowlunge":
-      return lowlunge;
-    case "pigeon":
-      return pigeon;
+    case "lowlunge_right":
+      return lowlunge_right;
+    case "lowlunge_left":
+      return lowlunge_left;
+    case "pigeon_right":
+      return pigeon_right;
+    case "pigeon_left":
+      return pigeon_left;
     case "plow":
       return plow;
-    case "pyramid":
-      return pyramid;
-    case "reverswarrior":
-      return reverswarrior;
+    case "pyramid_right":
+      return pyramid_right;
+    case "pyramid_left":
+      return pyramid_left;
+    case "reverswarrior_right":
+      return reverswarrior_right;
+    case "reverswarrior_left":
+      return reverswarrior_left;
     case "seatedforwardfold":
       return seatedforwardfold;
     case "standingforwardfold":
       return standingforwardfold;
-    case "triangle":
-      return triangle;
+    case "triangle_right":
+      return triangle_right;
+    case "triangle_left":
+      return triangle_left;
     case "updog":
       return updog;
-    case "warrior1":
-      return warrior1;
-    case "warrior3":
-      return warrior3;
-    case "warrior2":
-      return warrior2;
+    case "warrior1_right":
+      return warrior1_right;
+    case "warrior1_left":
+      return warrior1_left;
+    case "warrior3_right":
+      return warrior3_right;
+    case "warrior3_left":
+      return warrior3_left;
+    case "warrior2_right":
+      return warrior2_right;
+    case "warrior2_left":
+      return warrior2_left;
     case "wheel":
       return wheel;
-    case "wildthing":
-      return wildthing;
+    case "wildthing_right":
+      return wildthing_right;
+    case "wildthing_left":
+      return wildthing_left;
     case "shoulderstand":
       return shoulderstand;
-    case "sideplank":
-      return sideplank;
+    case "sideplank_right":
+      return sideplank_right;
+    case "sideplank_left":
+      return sideplank_left;
     case "straddlesplit":
       return straddlesplit;
     case "sphinx":
       return sphinx;
-    case "splits":
-      return splits;
+    case "splits_right":
+      return splits_right;
+    case "splits_left":
+      return splits_left;
   }
 }
 
